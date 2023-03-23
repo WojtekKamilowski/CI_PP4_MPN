adcef26978d81f9ef81175582dea563ad6d22e24
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-03-23 22:07:55 +0000

    Update and migrate database models

--- stocklist/models.py
+++ stocklist/models.py
@@ -4,21 +4,32 @@ from cloudinary.models import CloudinaryField
 
 class Stocklist(models.Model):
     name = models.CharField(max_length=150)
-    user = models.ForeignKey(User,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stock_list')
 
 class Storagespace(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    stocklist = models.ForeignKey(Stocklist)
+    stocklist = models.ForeignKey(Stocklist, on_delete=models.CASCADE, related_name='storage_space')
     temp = models.IntegerField(default=21)
 
+PIECE = 'PC'
+LITRE = 'L'
+KILOGRAM = 'KG'
+BOTTLE = 'BOTTLE'
+CUP = 'CUP'
+PACK = 'PACK'
+
 UOM_CHOICES = [
-        (PIECES, 'pcs'),
-        (LITRES, 'L'),   
+        (PIECE, 'pc'),
+        (LITRE, 'L'),
+        (KILOGRAM, 'kg'),
+        (BOTTLE, 'bottle'),
+        (CUP, 'cup'),
+        (PACK, 'pack'),
     ]
 
 class Stockitem(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    storage = models.ManyToManyField(Storagespace, related_name='name')
+    storage = models.ManyToManyField(Storagespace)
     expiry_date = models.DateField(null=True, blank=True)
     remarks = models.TextField()
     product_image = CloudinaryField('image', default='placeholder')
@@ -26,6 +37,13 @@ class Stockitem(models.Model):
     max_temp = models.IntegerField(default= 30)
     quantity = models.IntegerField(default= 1)
     uom = models.CharField(
-        choices= UOM_CHOICES,
-        default=PIECES,
+        max_length=50,
+        choices=UOM_CHOICES,
+        default=PIECE,
     )
+
+    class Meta:
+        ordering: ['-expiry_date']
+    
+    def __str__(self):
+        return self.name
