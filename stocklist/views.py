debdbe1e4a60fce763a3a32f145aed2c0d284bbe
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-03-26 11:22:00 +0000

    Add home view function

--- stocklist/views.py
+++ stocklist/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    template_name = 'index.html'
+    context = {}
+    return render(request, template_name, context)
