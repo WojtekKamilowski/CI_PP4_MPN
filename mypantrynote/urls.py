7ebc074d3007fc169d7864d45200b6b08f98f292
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-03-28 21:02:43 +0000

    Add Allauth url and update settings.py

--- mypantrynote/urls.py
+++ mypantrynote/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('stocklist.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
