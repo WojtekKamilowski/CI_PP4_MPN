ab684850eb54f6875d6aef9878ece0dfbda7c364
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-04-01 11:08:28 +0000

    Create PantryStocklist class view

--- stocklist/views.py
+++ stocklist/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Stocklist
 
 def home(request):
     template_name = 'index.html'
     context = {}
     return render(request, template_name, context)
+
+
+class PantryStocklist(generic.ListView):
+    model = Stocklist
+    template_name = 'list.html'
