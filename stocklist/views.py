b124637d51108fdc27c7aeccfab1bcaa4b63908e
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-04-02 12:13:01 +0000

    Create PantryStoragespaces view

--- stocklist/views.py
+++ stocklist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic, View
-from .models import Stocklist
+from .models import Stocklist, Storagespace
 
 
 def home(request):
@@ -8,7 +8,14 @@ def home(request):
     context = {}
     return render(request, template_name, context)
 
+
 class PantryStocklist(generic.ListView):
     model = Stocklist
     queryset = Stocklist.objects.all()
     template_name = 'list.html'
+
+
+class PantryStoragespaces(generic.ListView):
+    model = Storagespace
+    queryset = Storagespace.objects.all()
+    template_name = 'spaces.html'
