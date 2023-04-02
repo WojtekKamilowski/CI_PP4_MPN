408f43660db17d74e68ddfc147e6a91438f89a4b
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-04-02 12:21:16 +0000

    Create urlpattern for spaces

--- stocklist/urls.py
+++ stocklist/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('list/', views.PantryStocklist.as_view(), name='list'),
+    path('spaces/', views.PantryStoragespaces.as_view(), name='spaces'),
 ]
