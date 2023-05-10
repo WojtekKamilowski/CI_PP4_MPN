5ff663017cae81b2c901f59b020d98cad36f7304
WojtekKamilowski <>
2023-05-10 21:39:54 +0000

    Review code and seek improvements

--- stocklist/forms.py
+++ stocklist/forms.py
@@ -5,33 +5,43 @@ from .models import Stocklist, Storagespace, Stockitem
 class StocklistForm(forms.ModelForm):
     class Meta:
         model = Stocklist
-        fields = ['name']
+        fields = ["name"]
 
 
 class StorageForm(forms.ModelForm):
     class Meta:
         model = Storagespace
-        fields = ['storage_name','temp']
+        fields = ["storage_name", "temp"]
 
 
 class DateInput(forms.DateInput):
     """
     Datepicker found on Stackoverflow
     """
-    input_type = 'date'
+
+    input_type = "date"
 
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Stockitem
-        fields = ['item_name','expiry_date','storage','remarks','min_temp','max_temp','quantity','uom']
+        fields = [
+            "item_name",
+            "expiry_date",
+            "storage",
+            "remarks",
+            "min_temp",
+            "max_temp",
+            "quantity",
+            "uom",
+        ]
         # Datepicker found on Stackoverflow
         widgets = {
-            'expiry_date': DateInput(),
+            "expiry_date": DateInput(),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        user = kwargs.get('stocklist')
+        user = kwargs.get("stocklist")
         if user:
-            fields['storage'].queryset = user.storage_set.all()
+            fields["storage"].queryset = user.storage_set.all()
