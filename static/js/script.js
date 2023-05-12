8c1bac1fab9964a1f9f82e940539c14d38448dcb
WojtekKamilowski <>
2023-05-12 20:37:06 +0000

    Add js mouseenter/out events

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,33 @@ $(document).ready(function () {
         alert.close();
     }, 3000);
 });
+
+// Changes color of create_list button when mouse on
+$("#create_list").on("mouseenter", function () {
+    $("#create_list").css("background-color", "#CCC731");
+});
+
+// Changes color of create_list button when mouse out
+$("#create_list").on("mouseout", function () {
+    $("#create_list").css("background-color", "#55D54E");
+});
+
+// Changes color of create_storage button when mouse on
+$("#create_storage").on("mouseenter", function () {
+    $("#create_storage").css("background-color", "#CCC731");
+});
+
+// Changes color of create_storage button when mouse out
+$("#create_storage").on("mouseout", function () {
+    $("#create_storage").css("background-color", "#55D54E");
+});
+
+// Changes color of create_item button when mouse on
+$("#create_item").on("mouseenter", function () {
+    $("#create_item").css("background-color", "#CCC731");
+});
+
+// Changes color of create_item button when mouse out
+$("#create_item").on("mouseout", function () {
+    $("#create_item").css("background-color", "#55D54E");
+});
