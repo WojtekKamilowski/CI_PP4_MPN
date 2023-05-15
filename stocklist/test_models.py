02769345d78965dd5200f67e825e6c891b878e79
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-05-15 21:04:13 +0000

    Add test to create a new storagespace

--- stocklist/test_models.py
+++ stocklist/test_models.py
@@ -17,8 +17,30 @@ class TestModels(TestCase):
 
         stocklist = Stocklist.objects.create(
             name='Test Stocklist',
-            user=self.user
+            user=self.user,
             )
         self.assertEqual(stocklist.name, 'Test Stocklist')
         self.assertTrue(stocklist.user)
+    
+    def test_new_storagespace(self):
+        """
+        Test to create a new storagespace
+        Checks for the storage_name, stocklist & temp
+        """
+        # From Stackoverflow
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
 
+        stocklist = Stocklist.objects.create(
+            name='Test Stocklist',
+            user=self.user,
+            )
+        
+        storagespace = Storagespace.objects.create(
+            storage_name='Test Storagespace',
+            stocklist=stocklist,
+            temp=21
+            )
+        self.assertEqual(storagespace.storage_name,'Test Storagespace')
+        self.assertTrue(storagespace.stocklist,'Test Stocklist')
+        self.assertEqual(storagespace.temp, 21)
