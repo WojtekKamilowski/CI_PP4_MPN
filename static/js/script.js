d7e8d77e59c813fff67188de02558819c7fa741d
WojtekKamilowski <>
2023-05-15 06:39:13 +0000

    Simplify mouseon/out code

--- static/js/script.js
+++ static/js/script.js
@@ -7,32 +7,12 @@ $(document).ready(function () {
     }, 3000);
 });
 
-// Changes color of create_list button when mouse on
-$("#create_list").on("mouseenter", function () {
-    $("#create_list").css("background-color", "#CCC731");
+// Changes color of create_box buttons when mouse on
+$(".create_box").on("mouseenter", function () {
+    $(".create_box").css("background-color", "#CCC731");
 });
 
-// Changes color of create_list button when mouse out
-$("#create_list").on("mouseout", function () {
-    $("#create_list").css("background-color", "#55D54E");
-});
-
-// Changes color of create_storage button when mouse on
-$("#create_storage").on("mouseenter", function () {
-    $("#create_storage").css("background-color", "#CCC731");
-});
-
-// Changes color of create_storage button when mouse out
-$("#create_storage").on("mouseout", function () {
-    $("#create_storage").css("background-color", "#55D54E");
-});
-
-// Changes color of create_item button when mouse on
-$("#create_item").on("mouseenter", function () {
-    $("#create_item").css("background-color", "#CCC731");
-});
-
-// Changes color of create_item button when mouse out
-$("#create_item").on("mouseout", function () {
-    $("#create_item").css("background-color", "#55D54E");
+// Changes color of create_box buttons when mouse out
+$(".create_box").on("mouseout", function () {
+    $(".create_box").css("background-color", "#55D54E");
 });
