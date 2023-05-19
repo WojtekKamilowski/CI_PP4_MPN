064182de4ae03187d4d18829cac0a9f173f23fc1
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-05-19 12:15:32 +0000

    Update PantryStoragespaces view

--- stocklist/views.py
+++ stocklist/views.py
@@ -88,7 +88,7 @@ class PantryStoragespaces(generic.ListView):
         return (
             Storagespace.objects.select_related("stocklist")
             .filter(stocklist__user=self.request.user)
-            .order_by("-storage_updated_on")
+            .order_by("storage_updated_on")
         )
 
     def add_storagespace(request):
