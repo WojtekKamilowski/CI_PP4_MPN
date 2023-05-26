a2c9b6b56e9935987f47e33ff1d4f7604b8a6fd8
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-05-26 08:11:53 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,21 +8,29 @@ $(document).ready(function () {
 });
 
 // Changes color of create_box buttons when mouse on
-$(".create_box").on("mouseenter", function () {
-    $(".create_box").css("background-color", "#CCC731");
+$(document).ready(function (){ 
+    $(".create_box").on("mouseenter", function () {
+        $(".create_box").css("background-color", "#CCC731");
+    });
 });
 
 // Changes color of create_box buttons when mouse out
-$(".create_box").on("mouseout", function () {
-    $(".create_box").css("background-color", "#55D54E");
+$(document).ready(function (){ 
+    $(".create_box").on("mouseout", function () {
+        $(".create_box").css("background-color", "#55D54E");
+    });
 });
 
 // Hihghlights items table row when mouse enters
-$(".item_details").on("mouseenter", function () {
-    $(this).addClass("row_highlight");
+$(document).ready(function (){
+    $(".item_details").on("mouseenter", function () {
+        $(this).addClass("row_highlight");
+    });
 });
 
 // Removes hihghlight of items table row when mouse leaves
-$(".item_details").on("mouseout", function () {
-    $(this).removeClass("row_highlight");
+$(document).ready(function (){
+    $(".item_details").on("mouseout", function () {
+        $(this).removeClass("row_highlight");
+    });
 });
